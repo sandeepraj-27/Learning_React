57a4a21c17a9150f82e0a98258dbcac1f10ae860
sandeepraj-27 <syetirajula@gmail.com>
2024-02-18 22:18:37 +0530

    i have used useState in this!!!

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>react app with vite</h1>;
+  let [counter, setCounter] = useState(10);
+
+  const addValue = () => {
+    if (counter < 20) setCounter(counter + 1);
+  };
+  const decreaseValue = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>react app with vite</h1>
+      <h3>Counter : {counter}</h3>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={decreaseValue}>Decrease Value</button>
+    </>
+  );
 }
 
 export default App;
